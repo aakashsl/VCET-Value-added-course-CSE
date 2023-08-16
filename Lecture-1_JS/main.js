02ba7a0824941d70ad20042a96c3aaccc5c14436
Aakash SL <slaakash06@gmail.com>
2023-08-16 06:04:56 +0530

    Css , JS

---
+++ Lecture-1_JS/main.js
@@ -0,0 +1,87 @@
+var a=10;
+var d=50,e=60;
+var b="Aakash";
+var c=100.96;
+let name;
+const abc=20;
+name = "Aakash"
+ 
+
+d+=12 //test
+e/=3
+
+console.log(d)
+console.log(e)
+console.log("Hello World")
+console.log(a)
+console.log(b)
+console.log(c)
+console.log(52+50)
+console.log(52-3)
+console.log(name)
+
+//display html
+
+var n=false;
+console.log(n)
+var name1="Aakash"
+        document.getElementById("test").innerHTML=(name1)
+
+//if
+
+if(n){
+    console.log("HI")
+}
+else
+{
+    console.log("sad")
+}
+
+//if sample
+
+let shop = 499;
+if(shop>=500){
+    shop-=10;
+    console.log(shop)
+}
+else{
+    console.log(shop)
+}
+
+let y=5;
+//switch
+
+switch(y){
+    case 1:
+        console.log("hi");
+        break;
+    case 2:
+        console.log("hello");
+        break;
+    case 3:
+        console.log("World");
+        break;
+    default:
+        console.log("Default");
+}
+
+//Ternary
+
+let age=40;
+let isAdult = (age<18) ? "Young" : "Old"
+console.log(isAdult)
+
+//for 
+
+let i;
+for(i=1 ; i<5; i++){
+    console.log(i);
+
+}
+
+//while
+let x=10;
+while(x<20){
+    console.log("yes");
+    break;
+}
